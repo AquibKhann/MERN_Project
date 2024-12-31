6b127ef659abaae8cf0e0d3b9fbdb49256693b1b
Aquib Khan <aquibkhanstud@gmail.com>
2024-12-31 13:26:37 +0530

    donw eith the basic setup of login and register components

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth-slice';
-import secondSliceReducer from './secondSlice';
+
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
-        //import secondSliceReducer from './secondSlice';: import secondSliceReducer from './secondSlice'; Reducer
     },
 });
 
